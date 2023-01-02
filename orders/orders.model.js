4c3c184dbac7a303b7e227806c8d6a5d1eb0f2e2
Harsh Dwivedi <harshdvd10@gmail.com>
2023-01-02 23:27:55 +0530

    Modularizing Queries and Data

---
+++ orders/orders.model.js
@@ -0,0 +1,24 @@
+module.exports = [
+    {
+        date: '2005-05-05',
+        subtotal: 90.22,
+        items: [
+            {
+                product: {
+                    id: 'redshoe',
+                    description: 'Old Red Shoe',
+                    price: 45.11
+                },
+                quantity: 2
+            },
+            {
+                product: {
+                    id: 'bluejean',
+                    description: 'Old Blue Jean',
+                    price: 55.10
+                },
+                quantity: 2
+            }
+        ]
+    }
+]
