ffa4fafe77273dd3bd5748e726361985b439ee2c
Harsh Dwivedi <harshdvd10@gmail.com>
2023-01-03 01:42:12 +0530

    Added Mutations

--- products/products.model.js
+++ products/products.model.js
@@ -2,12 +2,14 @@ const products =  [
     {
         id: 'redshoe',
         description: 'Red Shoe',
-        price: '42.12'
+        price: '42.12',
+        reviews: []
     },
     {
         id: 'bluejean',
         description: 'Blue Jeans',
-        price: '55.55'
+        price: '55.55',
+        reviews: []
     },
 ]
 
@@ -27,8 +29,33 @@ function getProductsById(id) {
     })
 }
 
+function addNewProduct(id, description, price) {
+    const newProduct = {
+        id,
+        price,
+        description,
+        reviews: []
+    }
+    products.push(newProduct)
+    return newProduct
+}
+
+function addNewProductReview(id, rating, comment) {
+    const matchedProduct = getProductsById(id)
+    if(matchedProduct) {
+        const newProductReview = {
+            rating,
+            comment
+        }
+        matchedProduct.reviews.push(newProductReview)
+        return newProductReview
+    }
+}
+
 module.exports = {
     getAllProducts,
     getProductsByPrice,
-    getProductsById
+    getProductsById,
+    addNewProduct,
+    addNewProductReview
 }
